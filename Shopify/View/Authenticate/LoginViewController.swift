2d2f3598ef4acafc975cd91ce531cca05486cdc8
ayasalman <ayas23830@gmail.com>
2023-02-23 23:05:14 +0200

    newBranch

--- Shopify/View/Authenticate/LoginViewController.swift
+++ Shopify/View/Authenticate/LoginViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var topView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+      /* self.topView.layer.masksToBounds = true
+    self.topView.layer.cornerRadius = self.topView.frame.size.width/2
+        
+        self.topView.layer.cornerRadius = self.topView.frame.size.width/CGFloat(2)
+        
+       // self.topView.layer.contentsScale = self.topView.frame.size.height/2
+      //  self.topView.layer.
+           
+       // let layershape = CAShapeLayer()
+       // let circlePath = UIBezierPath(arcCenter: center, radius: 200, startAngle: 6.28, endAngle: 3 * CGFloat.pi , clockwise: true)
+        */
+        
+      /*  let circlePath = UIBezierPath(arcCenter: center, radius: 200, startAngle: 6.28, endAngle: 3 * CGFloat.pi , clockwise: true)
+        layershape.path = circlePath.cgPath
+        view.layer.addSublayer(layershape)
+        */
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
