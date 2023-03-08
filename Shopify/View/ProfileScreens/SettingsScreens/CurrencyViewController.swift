b3dc7cbfefd0555e834d6eb853c6ce9ec72c62cb
fatma <fatima25952@gmail.com>
2023-03-09 01:44:16 +0200

    currency

--- Shopify/View/ProfileScreens/SettingsScreens/CurrencyViewController.swift
+++ Shopify/View/ProfileScreens/SettingsScreens/CurrencyViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class CurrencyViewController: UIViewController {
     
-    
-    var userdef :  UserDefaults?
+    var userdef = UserDefaults.standard
    // userdef.setValue(false, forKey: "firstTime")
 
     @IBOutlet weak var bgFrame: UIView!
@@ -19,21 +18,18 @@ class CurrencyViewController: UIViewController {
     
     @IBAction func selectUSD(_ sender: Any) {
         setCurrencyImage(currency : USD)
-        // userdef.setValue(false, forKey: "firstTime")
+        userdef.setValue(false, forKey: "currency")
 
     }
     
     @IBAction func selectGBP(_ sender: Any) {
         setCurrencyImage(currency : GBP)
-      putCurrency(currency: "f")
+        userdef.setValue(true, forKey: "currency")
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         StyleHelper.bgFrameStyle(frame: bgFrame)
-          userdef = UserDefaults()
     }
     
 }
@@ -45,35 +41,4 @@ extension CurrencyViewController
         USD.setImage(UIImage(systemName: "dot.circle"), for: .normal)
         currency.setImage(UIImage(systemName: "dot.circle.fill"), for: .normal)
     }
-    func putCurrency(currency : String){
-        guard let url = URL(string: "https://48c475a06d64f3aec1289f7559115a55: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@ios-q2-new-capital-admin2-2022-2023.myshopify.com/admin/api/2023-01/customers/6818756165936.json") else{
-                return
-            }
-            var request = URLRequest(url :url)
-            request.httpMethod = "PUT"
-            
-            request.httpShouldHandleCookies = false
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            let body : [String : Any] = [
-                "customer": [
-                    "id" : 6818756165936,
-                    "currency": "Fatma"
-                        
-                ]
-            ]
-            request.httpBody = try? JSONSerialization.data(withJSONObject: body,options: .fragmentsAllowed)
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data else{
-                    return
-                }
-                do{
-                    print("success\(response)")
-                }
-                catch{
-                }
-            }
-            task.resume()
-        }
- 
 }
