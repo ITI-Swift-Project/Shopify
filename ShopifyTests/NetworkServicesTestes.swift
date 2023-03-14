fcd0908cb423ea6e1efdaaece833c3dc651869b4
ahmad ayman mansour <ahmed257ayman@gmail.com>
2023-03-14 23:57:37 +0200

    complet test for nwtwork , and test CoreDataManager (Cart , WishList)

--- ShopifyTests/NetworkServicesTestes.swift
+++ ShopifyTests/NetworkServicesTestes.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import OHHTTPStubs
 @testable import Shopify
 
 final class NetworkServicesTestes: XCTestCase {
@@ -21,18 +22,18 @@ final class NetworkServicesTestes: XCTestCase {
     
     //MARK: - Test Fetching Methods
     
-    
-//    func testFetch() {
-//        let expectaion = expectation(description: "Waiting for the API to get Data")
-//        
-//        let url = URL(string: "\(NetworkService.base_url)custom_collections.json")
-//        NetworkService.fetch(url: url) { result in
-//            self.brands = result
-//            XCTAssertNotEqual(self.brands.smart_collections?.count, 0, "API Failed")
-//            expectaion.fulfill()
-//        }
-//        waitForExpectations(timeout: 5 , handler: nil)
-//    }
+    func testFetch() {
+        let expectation = XCTestExpectation(description: "Fetch request completed")
+        
+        let url = URL(string: "\(NetworkService.base_url)custom_collections.json")!
+        
+        NetworkService.fetch(url: url) { (result: Brands?) in
+            XCTAssertNotNil(result, "Result should not be nil")
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
     
     
     func testGetDiscountCodes() {
@@ -115,6 +116,81 @@ final class NetworkServicesTestes: XCTestCase {
         waitForExpectations(timeout: 5 , handler: nil)
     }
     
+    func testGetCustomerAddresses() {
+            let stubbedResponse = Customer()
+            let url = "https://example.com/api"
+            stub(condition: isAbsoluteURLString(url)) { _ in
+                let data = try! JSONEncoder().encode(stubbedResponse)
+                return HTTPStubsResponse(data: data, statusCode: 200, headers: nil)
+            }
+            
+            let expectation = self.expectation(description: "Completion handler invoked")
+        Temp.getCustomerAddresses(url: url) { result in
+               
+                expectation.fulfill()
+            }
+            
+            waitForExpectations(timeout: 5, handler: nil)
+            
+            HTTPStubs.removeAllStubs()
+        }
     
     //MARK: - Test POST Methods
+    func testPostData() {
+        let expectation = XCTestExpectation(description: "POST request completed")
+        
+        let url = URL(string: "\(NetworkService.base_url)products.json")!
+        let parameters = ["key": "value"]
+        
+        NetworkService.postData(urlEndPoint: url, parameter: parameters) { result in
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data, "Data should not be nil")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("POST request failed with error: \(error.localizedDescription)")
+            }
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    
+    func testPostWithError() {
+            let stubbedResponse = ["key": "value"]
+            let url = "\(NetworkService.base_url)products.json"
+            stub(condition: isAbsoluteURLString(url)) { _ in
+                let data = try! JSONSerialization.data(withJSONObject: stubbedResponse, options: .prettyPrinted)
+                return HTTPStubsResponse(data: data, statusCode: 200, headers: nil)
+            }
+            
+            let parameters = ["foo": "bar"]
+            let expectation = self.expectation(description: "Completion handler invoked")
+            NetworkService.postWithError(url: url, parameters: parameters) { result in
+                XCTAssertEqual(result as! [String : String], stubbedResponse)
+                expectation.fulfill()
+            }
+            waitForExpectations(timeout: 5, handler: nil)
+            HTTPStubs.removeAllStubs()
+        }
+
+        //MARK: - PUT Methods
+    func testPutWithError() {
+            let stubbedResponse = ["key": "value"]
+            let url = "\(NetworkService.base_url)products/8162372452656.json"
+            stub(condition: isAbsoluteURLString(url)) { _ in
+                let data = try! JSONSerialization.data(withJSONObject: stubbedResponse, options: .prettyPrinted)
+                return HTTPStubsResponse(data: data, statusCode: 200, headers: nil)
+            }
+            let parameters = ["foo": "bar"]
+            let expectation = self.expectation(description: "Completion handler invoked")
+            NetworkService.putWithError(url: url, parameters: parameters) { result in
+                XCTAssertEqual(result as! [String : String], stubbedResponse)
+                expectation.fulfill()
+            }
+            waitForExpectations(timeout: 5, handler: nil)
+            HTTPStubs.removeAllStubs()
+        }
+
+    
 }
